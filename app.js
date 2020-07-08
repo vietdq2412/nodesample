a4e2fab643938b833367e45a2e94908b6acb4c2f
Viet <vietdq2412@gmail.com>
2020-07-08 17:10:02 +0700

    aaa

---
+++ app.js
@@ -0,0 +1,29 @@
+console.log(3 + 5 + 9)
+i = 1
+for (i = i; i < 10; i++) {
+    console.log(i)
+}
+name = 'asasasa asas fsds';
+console.log(name.length);
+n = 0;
+
+function nbinh(n) {
+    return n * n;
+}
+
+console.log(nbinh(2));
+
+function chuvi(a, b) {
+    return (a + b) * 2;
+}
+
+
+console.log(chuvi(3, 4))
+
+function bangcuuchuong(n) {
+    for (i = 1; i < 10; i++) {
+        console.log(n * i);
+    }
+}
+
+bangcuuchuong(2);
